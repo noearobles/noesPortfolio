4bfbfb8aecfd8914691d9ef1804602e995bb7b8a
noaarobles <roblesnoe1030@gmail.com>
2022-11-13 21:38:07 -0600

    fixed mobile font

--- scrpipt.js
+++ scrpipt.js
@@ -22,7 +22,7 @@ function init() {
 init();
 
 
-function openCity(evt, cityName) {
+function openJob(evt, jobName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -39,7 +39,7 @@ function openCity(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(jobName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
